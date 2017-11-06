052ab0e364995517b911e4106c8e45b17028fa1a
Derrick Ike <derrickike@Derricks-MBP.san.rr.com>
2017-11-05 17:08:12 -0800

    Refactored code for todos app

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    match: [/@/, 'Invalid email address']
+  }
+});
+
+module.exports = {User};
