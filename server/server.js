6b37c705fe832adfe9a644815a8f798110d2b972
Derrick Ike <derrickike@Derricks-MBP.san.rr.com>
2017-11-06 15:15:06 -0800

    GET /todos/id: route

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.get('/todos/:id', (req, res) => {
           }
     res.send(todo);
   }, (e) => {
-    res.status(404).send()
+    res.status(400).send()
   });
 
   //validate id -- if not found respond with code 404
