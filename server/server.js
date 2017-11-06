78112b21412d289c3e2932956f07a41503a1a01d
Derrick Ike <derrickike@Derricks-MBP.san.rr.com>
2017-11-05 18:58:15 -0800

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 //active port
 var activePort = 3000;
 
+//post route
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -21,6 +22,15 @@ app.post('/todos', (req, res) => {
 
   todo.save().then((doc) =>{
     res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+//get route
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
